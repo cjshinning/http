340bfd0f297014489e3743be6fcf9cd434b786a1
cjshinning <784319417@qq.com>
2018-05-31 15:32:15 +0800

    cache-control

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ var fs = require('fs')
 http.createServer(function(request,respose){
     console.log('request come', request.url)
 
-    const html = fs.readFileSync('test.html', 'utf8')
-    respose.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
+    if(request.url === '/') {
+        const html = fs.readFileSync('test.html', 'utf8')
+        respose.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        respose.end(html)
+    }
 
-    respose.end(html)
+    if(request.url === '/script.js') {
+        respose.writeHead(200, {
+            'Content-Type': 'text/javascript',
+            'Cache-Control': 'max-age=200'
+        })
+        respose.end('console.log("script loaded twice")')
+    }
+
 }).listen(8888)
 
 console.log('server listening on 8888')
--- server2.js
+++ server2.js
@@ -4,7 +4,10 @@ http.createServer(function(request,respose){
     console.log('request come', request.url)
 
     respose.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'X-Test-Cors',
+        'Access-Control-Allow-Methods': 'POST, PUT, Delete',
+        'Access-Control-Max-Age': '1000'
     })
     respose.end('123')
 }).listen(8887)
